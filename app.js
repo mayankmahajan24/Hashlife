99454a1b2ba810ebcb38536d11be96ced2b9f671
Mayank Mahajan <mayank.mahajan@gmail.com>
2015-01-18 08:16:40 -0500

    Updated HTML and CSS

--- app.js
+++ app.js
@@ -93,10 +93,12 @@ app.get("/getkey", function (req, res) {
         if (results.length == 0)
             {
                 res.send({0: "failed"});
-            return;
+                res.end();
+               return;
         }
         else{
             res.send({0: results[0].pkey});
+            res.end();
             return;
             }
 
