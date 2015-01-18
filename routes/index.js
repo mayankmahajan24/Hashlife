37b992063ad176cfd4c5ad7fba456c24dc645b92
Mayank Mahajan <mayank.mahajan@gmail.com>
2015-01-18 08:24:21 -0500

    Updated HTML and CSS

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render("index", {});
 
- 	res.send({"0": "Hello World"});
+ 	res.write({"0": "Hello World"});
+ 	res.render("index", {});
+
 });
 
 module.exports = router;
