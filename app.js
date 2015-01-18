6754836973e4b7f9c61c08add243f093aa95efcd
Mayank Mahajan <mayank.mahajan@gmail.com>
2015-01-18 01:01:18 -0500

    debugging passcode

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.get('/register', function (req, res) {
     doc = {phone: req.query.phone, pkey: req.query.pkey};
     var fn = "+" + req.query.phone;
     mongoose.model('kode').find({phone: fn}, function(err, results) {
+        console.log(results[0]);
         codeshouldbe = results[0].code;
     });
     
