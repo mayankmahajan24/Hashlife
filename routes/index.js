5e89b89eb00a57a94ea76d9f96a73c1a8b9a357b
Mayank Mahajan <mayank.mahajan@gmail.com>
2015-01-17 22:10:52 -0500

    Started creating landing page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  //res.render('index', { title: 'Hello World' });
+  res.render('index', { title: 'Hello World' });
  	res.send({"0": "Hello World"});
 });
 
